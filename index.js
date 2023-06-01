c479ea1de1f7e8445ae8a742de4bcf87e3dad9b0
Christopher Lozzi <clozzi12@gmail.com>
2023-06-01 17:51:09 -0400

    fetch username + image + display w/ css

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ document.addEventListener('DOMContentLoaded', fetchVolunteer)
 function fetchVolunteer() {
     fetch("https://randomuser.me/api/")
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displaySubject(data));
+}
+
+function displaySubject(data) {
+    let volunteer = data.results[0];
+    let subjectName = (volunteer.name.first + " " + volunteer.name.last);
+    let heading = document.createElement("h1");
+    heading.innerHTML = subjectName;
+    randomSubject.appendChild(heading);
+
+    let subjectImg = document.createElement("img");
+    subjectImg.src = volunteer.picture.large;
+    randomSubject.appendChild(subjectImg);
 }
