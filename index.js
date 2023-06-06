efe6361f1bc69288addcb2b3cce80f4fd1a89d48
Christopher Lozzi <clozzi12@gmail.com>
2023-06-06 12:30:00 -0400

    added notes functionality in JS

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ function displaySubject(data) {
 
 document.getElementById("nextProfile").addEventListener("click", fetchVolunteer);
 
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      newNote(e.target["newNote"].value)
+      form.reset()
+    });
+  });
+  
+  function newNote(userNote) {
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', handleDelete);
+    btn.textContent = 'x';
+    li.textContent = `${userNote}`;
+    li.appendChild[btn];
+    document.querySelector('#myNotes').appendChild(li);
+  }
+  
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
