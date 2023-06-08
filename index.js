be0e08f5976c0ead017555f6d46d66176009092f
Christopher Lozzi <clozzi12@gmail.com>
2023-06-08 13:21:50 -0400

    click to add potential subject names -- img reference buggy

--- index.js
+++ index.js
@@ -39,15 +39,32 @@ function nextSubject(data) {
     let subjectName = (volunteer.name.first + " " + volunteer.name.last);
     let heading = document.createElement("h1");
     heading.innerHTML = subjectName;
+    heading.setAttribute("id", "usersName")
     randomSubject.appendChild(heading);
 //add image below the header
     let subjectImg = document.createElement("img");
+    subjectImg.setAttribute('id', 'subjImg');
     subjectImg.src = volunteer.picture.large;
     randomSubject.appendChild(subjectImg);
 //add country below image
     let subjCountry = document.createElement("p");
     subjCountry.innerHTML = volunteer.location.country;
     randomSubject.appendChild(subjCountry);
+}
+
+document.getElementById('addSubject').addEventListener('click', addToList);
+//need to clear original img OR access new img element (img elements add with each click so no reference point)
+function addToList() {
+    let heading = document.createElement('h1');
+    let newHeader = document.getElementById('usersName');
+    heading.innerHTML = newHeader.innerHTML;
+    subjectList.appendChild(heading);
+
+    console.log(subjImg);
+    let subjectImg = document.createElement("img");
+    subjectImg.setAttribute('id', 'subjImg');
+    subjectImg.src = subjImg.src;
+    subjectList.appendChild(subjectImg);
 }
 
 //on dom load, add submit functionality to form and run f on input value
